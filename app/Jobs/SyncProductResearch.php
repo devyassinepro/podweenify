32d63f7f04e743fea3edad6711a03441780e143f
Touzani <touzani@macbook-pro-de-touzani.home>
2023-10-25 01:28:53 +0200

    database stores

---
+++ app/Jobs/SyncProductResearch.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
+use GuzzleHttp\Client;
+use App\Models\stores;
+use App\Models\Product;
+
+set_time_limit(0);
+
+
+class SyncProductResearch implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $store;
+
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($store)
+    {
+        $this->store = $store;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+
+        $domain = $this->store;
+        // Use try-catch for error handling
+        try {
+
+            // $stores = stores::where('url', $domain)->first();
+            $stores =  DB::table('stores')->where('url', $domain)->first();
+            // DB::table('products')
+            if($stores){
+
+            }else{
+                $opts = array('http' => array('header' => "User-Agent: MyAgent/1.0\r\n"));
+                $context = stream_context_create($opts);
+                $meta = file_get_contents($domain.'meta.json', false, $context);
+        
+                // Check if the JSON content is valid
+                if ($meta === false) {
+                    echo "Failed to retrieve data from $modifiedUrl";
+                } else {
+                    $metas = json_decode($meta);
+                    if (json_last_error() === JSON_ERROR_NONE) {
+                        $totalproducts = $metas->published_products_count;
+                        
+                        // echo "Total products for $site: $totalproducts<br>";
+                        
+                        $store_id = DB::table('stores')->insertGetId(
+                            ['url' => $domain,
+                            'name' => $metas->name,
+                            'status' => 0,
+                            'sales' => 0,
+                            'revenue' => 0,
+                            'city' => $metas->city,
+                            'country' => $metas->country,
+                            'currency' => $metas->currency,
+                            'shopifydomain' => $metas->myshopify_domain,
+                            'allproducts' => $metas->published_products_count,
+                            'created_at' => now(),
+                            'updated_at' => now(),
+                            'user_id' => 0
+                            ]
+                        );
+                        
+                        if($totalproducts<=250){
+                            $this->createstore($domain,$store_id,1);
+
+
+                        }else if($totalproducts<=500){
+                            for ($i = 1; $i <= 2; $i++) {
+                                $this->createstore($domain,$store_id,$i);
+
+                            }
+                         }else if($totalproducts<=750){
+                            for ($i = 1; $i <= 3; $i++) {
+                                $this->createstore($domain,$store_id,$i);
+
+                            }
+                        }
+                        else if($totalproducts<=1000 || $totalproducts>1000){
+                            for ($i = 1; $i <= 4; $i++) {
+                                $this->createstore($domain,$store_id,$i);
+                            }
+                        }
+
+                       
+                    } else {
+                        echo "Failed to decode JSON from $modifiedUrl: " . json_last_error_msg();
+                    }
+                }
+            }
+            } catch (Exception $e) {
+                echo "An error occurred: " . $e->getMessage();
+            }
+        
+    }
+
+
+    public function createstore ($store ,$store_id, $i){
+
+
+        try {
+                    $opts = array('http'=>array('header' => "User-Agent:MyAgent/1.0\r\n"));
+                    $context = stream_context_create($opts);
+                    $html = file_get_contents($store.'products.json?page='.$i.'&limit=250',false,$context);
+                    $products = json_decode($html)->products;
+                    foreach ($products as $product) {
+
+                        if(isset($product->variants[0]->price)){
+                            $price= $product->variants[0]->price;
+                        }else{
+                            $price=0;
+                        }
+                        if(isset($product->images[0]->src)){
+                            $image= $product->images[0]->src;
+                        }else{
+                            $image="default";
+                        }
+
+                        $timeconvert = strtotime($product->updated_at);
+                        $totalsales = 0;
+                        $urlproduct = $store.'products/'.$product->handle;
+                        Product::firstOrCreate([
+                            "id" => $product->id,
+                            "title" => $product->title,
+                            "timesparam" => $timeconvert,
+                            "prix" => $price,
+                            "revenue" => 0,
+                            "stores_id" => $store_id,
+                            "url" => $urlproduct,
+                            "imageproduct" => $image,
+                            "favoris" => 0,
+                            "totalsales" => $totalsales,
+                            "todaysales" => 0,
+                            "yesterdaysales" => 0,
+                            "day3sales" => 0,
+                            "day4sales" => 0,
+                            "day5sales" => 0,
+                            "day6sales" => 0,
+                            "day7sales" => 0,
+                            "weeksales" => 0,
+                            "monthsales" => 0
+                        ]);
+            }
+
+        
+        } catch (Exception $e) {
+                    echo "An error occurred: " . $e->getMessage();
+        }    
+    }
+
+
+}
+
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,13 @@ use App\Jobs\ProcessApiStoreJob;
 use App\Jobs\ProcessCountproductsRevenue;
 use App\Jobs\ProcessCountStoresRevenue;
 use App\Jobs\Process24storesRevenue;
+use App\Jobs\ProcessProductResearch;
 use App\Models\Sales;
 use Illuminate\Support\Facades\DB;
 use Laravel\Horizon\Http\Controllers\HorizonController;
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+use Illuminate\Support\Facades\Log;
 
 
 use Illuminate\Http\Request;
@@ -102,6 +106,343 @@ Route::get('/testcountProducts',function (){
             DB::table('products')->where('id', $product)->update($productreqtoday);
       echo $product; echo '<br />';
 });
+
+
+
+// Start Queue every 4 Hours
+Route::get('/testaddnewproduct',function (){
+
+
+    try {
+        $store = Stores::select("*")
+        ->where('id','4')
+        ->first();
+        echo $store->url; echo '<br />';
+
+        $opts = array('http'=>array('header' => "User-Agent:MyAgent/1.0\r\n"));
+        $context = stream_context_create($opts);
+        $meta = file_get_contents($store->url.'meta.json',false,$context);
+        $metas = json_decode($meta);
+        $totalproductslive = $metas->published_products_count;
+
+        echo $metas->published_products_count; echo '<br />';
+
+
+        //to compare with database 
+        $storeproductsDB = DB::table('stores')->where('id', $store->id)->first();
+
+        echo $storeproductsDB->allproducts; echo '<br />';
+
+        if($totalproductslive != $storeproductsDB->allproducts){
+
+                for ($i = 1; $i <= 3; $i++) {
+                 
+                    $opts = array('http'=>array('header' => "User-Agent:MyAgent/1.0\r\n"));
+                    $context = stream_context_create($opts);
+                    $html = file_get_contents($store->url.'products.json?page='.$i.'&limit=250',false,$context);
+                    $products = json_decode($html)->products;
+  
+
+                    collect($products)->map(function ($product) use ($store){
+
+                        $productbd = DB::table('products')->where('id', '=', $product->id)->first();
+
+
+                        if (!$productbd) {
+
+                            if(isset($product->variants[0]->price)){
+                                $price= $product->variants[0]->price;
+                            }else{
+                                $price=0;
+                            }
+                            if(isset($product->images[0]->src)){
+                                $image= $product->images[0]->src;
+                            }else{
+                                $image="default";
+                            }
+
+                            $timeconvert = strtotime($product->updated_at);
+                            $totalsales = 0;
+
+                            $urlproduct = $store->url.'products/'.$product->handle;
+
+                            Product::firstOrCreate([
+                                "id" => $product->id,
+                                "title" => $product->title,
+                                "timesparam" => $timeconvert,
+                                "prix" => $price,
+                                "revenue" => 0,
+                                "stores_id" => $store->id,
+                                "url" => $urlproduct,
+                                "imageproduct" => $image,
+                                "favoris" => 0,
+                                "totalsales" => $totalsales,
+                                "todaysales" => 0,
+                                "yesterdaysales" => 0,
+                                "day3sales" => 0,
+                                "day4sales" => 0,
+                                "day5sales" => 0,
+                                "day6sales" => 0,
+                                "day7sales" => 0,
+                                "weeksales" => 0,
+                                "monthsales" => 0
+                            ]);
+                                        }
+                    });
+                        //update number of products
+                        $updatenumberofnewproduct = array(
+                            'allproducts' => $totalproductslive,
+                        );
+                        DB::table('stores')->where('id', $store->id)->update($updatenumberofnewproduct);
+                
+                        echo $totalproductslive; echo '<br />';
+        
+                }
+
+        }
+
+    } catch(\Exception $exception) {
+
+        Log::error($exception->getMessage());
+    }
+
+});
+
+
+Route::get('/removestore',function (){
+
+    $id =979;
+
+    Stores::where('id', $id)->delete();
+
+
+});
+
+Route::get('/bigdata',function (){
+
+        $storesurl = [
+            'bumpboxx.com',
+        ];
+
+        ProcessProductResearch::dispatch($storesurl);
+
+
+});
+
+Route::get('/product',function (){
+
+        $store =823;
+
+            // Configure Guzzle with proxy and timeout options
+            $client = new Client([
+                'timeout' => 10, // Set a reasonable timeout for requests
+                'proxy' => '', // Initialize with an empty proxy
+            ]);
+
+                $urls = DB::table('products')
+                ->where('stores_id', $store)
+                ->take(30)
+                ->pluck('url')
+                ->toArray();
+                
+            $requestCount = 0; // Initialize the request count
+            $proxies = [
+                        'http://fkmroqdf:e7bqbxml5sfv@38.154.227.167:5868',
+                        'http://fkmroqdf:e7bqbxml5sfv@185.199.229.156:7492',
+                        'http://fkmroqdf:e7bqbxml5sfv@185.199.228.220:7300',
+                        'http://fkmroqdf:e7bqbxml5sfv@185.199.231.45:8382',
+                        'http://fkmroqdf:e7bqbxml5sfv@188.74.210.207:6286',
+                        'http://fkmroqdf:e7bqbxml5sfv@188.74.183.10:8279',
+                        'http://fkmroqdf:e7bqbxml5sfv@188.74.210.21:6100',
+                        'http://fkmroqdf:e7bqbxml5sfv@45.155.68.129:8133',
+                        'http://fkmroqdf:e7bqbxml5sfv@154.95.36.199:6893',
+                        'http://fkmroqdf:e7bqbxml5sfv@45.94.47.66:8110'
+        
+                       // Add more proxy servers as needed
+               ];
+
+        foreach ($urls as $url) {
+        // Implement rate limiting logic here to ensure you don't exceed the rate limit
+
+        // Check if it's time to change the proxy (after every 6 requests)
+        if ($requestCount % 3 === 0) {
+            // Rotate to the next proxy server in your list
+            $currentProxy = $proxies[$requestCount / 3 % count($proxies)];
+            $client = new Client([
+                'timeout' => 10,
+                'proxy' => $currentProxy,
+            ]);
+
+            // Echo the current proxy URL
+        }
+
+        // Make an HTTP request using Guzzle
+        try {
+       $response = $client->get($url . '.json');
+       $html = $response->getBody()->getContents();
+
+       // Process the response as needed
+       if ($response->getStatusCode() === 200) {
+           $product = json_decode($html)->product;
+           // Process and update the product data in the database
+
+               $productbd = DB::table('products')->where('id', $product->id)->where('timesparam', '!=', strtotime($product->updated_at))->first();
+               if($productbd) {
+                // echo "Product Sale: " . $product->title . "<br>"."Update at : " . $product->updated_at."Using Proxy: $currentProxy\n";
+
+                // Assuming $product and $currentProxy are defined elsewhere in your code
+                $productTitle = $product->title;
+                $updatedAt = $product->updated_at;
+
+                $message = "Product Sale: $productTitle\nUpdate at: $updatedAt\nUsing Proxy: $currentProxy";
+
+                Log::info($message);
+                   $sales = $productbd->totalsales;
+                   $todaysalesupdate = $productbd->todaysales;
+
+                   $revenuenow = $productbd->revenue + $productbd->prix;
+                   $sales ++ ;
+                   $todaysalesupdate ++ ;
+                   //echo $sales;
+                   $timestt = strtotime($product->updated_at);
+                   $productreq = array(
+                       'title' => $product->title,
+                       'timesparam' => $timestt,
+                       'prix' => $product->variants[0]->price,
+                       'revenue' => $revenuenow,
+                       'stores_id' => $productbd->stores_id,
+                       'imageproduct' => $product->images[0]->src,
+                       'favoris' => $productbd->favoris,
+                       'todaysales' => $todaysalesupdate,
+                       'totalsales' => $sales,
+                       'updated_at' => Carbon::now()->format('Y-m-d'),
+                   );
+
+                   DB::table('products')->where('id', $productbd->id)->update($productreq);
+
+                   DB::table('sales')->insert([
+                       "product_id" => $productbd->id,
+                       "stores_id" => $productbd->stores_id,
+                       "prix" => $productbd->prix,
+                       'created_at' => Carbon::now()->format('Y-m-d'),
+                       'updated_at' => Carbon::now()->format('Y-m-d')
+                   ]);
+
+                   }
+       }
+   } catch (\Exception $e) {
+       // Handle exceptions (e.g., connection errors, timeouts) and log errors
+       Log::error($e->getMessage());
+   }
+
+   // Increment the request count
+   $requestCount++;
+
+   // Sleep for a brief moment between requests to avoid overwhelming the proxy and server
+   sleep(1); // Adjust this as needed
+}
+   
+});
+
+//test 1 store
+
+Route::get('/updateproducts',function (){
+
+    // $storeid = 467;
+
+    // $urls = DB::table('products')
+    //     ->where('stores_id', $storeid)
+    //     ->take(10)
+    //     ->pluck('url')
+    //     ->toArray();
+    
+    // // Loop through the URLs and fetch product information
+    // foreach ($urls as $url) {
+    //     $opts = array('http' => array('header' => "User-Agent:MyAgent/1.0\r\n"));
+    //     $context = stream_context_create($opts);
+    //     $html = file_get_contents($url.'.json', false, $context);
+    
+    //     if ($html !== false) {
+    //         $product = json_decode($html)->product;
+
+    //         $productbd = DB::table('products')->where('id', $product->id)->where('timesparam', '!=', strtotime($product->updated_at))->first();
+    //         if($productbd) {
+
+    //             $sales = $productbd->totalsales;
+    //             $todaysalesupdate = $productbd->todaysales;
+
+    //             $revenuenow = $productbd->revenue + $productbd->prix;
+    //             $sales ++ ;
+    //             $todaysalesupdate ++ ;
+    //             //echo $sales;
+    //             $timestt = strtotime($product->updated_at);
+    //             $productreq = array(
+    //                 'title' => $product->title,
+    //                 'timesparam' => $timestt,
+    //                 'prix' => $product->variants[0]->price,
+    //                 'revenue' => $revenuenow,
+    //                 'stores_id' => $productbd->stores_id,
+    //                 'imageproduct' => $product->images[0]->src,
+    //                 'favoris' => $productbd->favoris,
+    //                 'todaysales' => $todaysalesupdate,
+    //                 'totalsales' => $sales,
+    //                 'updated_at' => Carbon::now()->format('Y-m-d'),
+    //             );
+
+    //             DB::table('products')->where('id', $productbd->id)->update($productreq);
+
+    //             DB::table('sales')->insert([
+    //                 "product_id" => $productbd->id,
+    //                 "stores_id" => $productbd->stores_id,
+    //                 "prix" => $productbd->prix,
+    //                 'created_at' => Carbon::now()->format('Y-m-d'),
+    //                 'updated_at' => Carbon::now()->format('Y-m-d')
+    //             ]);
+
+    //             }
+    //     } else {
+    //         echo "Failed to fetch data from URL: " . $url . "<br>";
+    //     }
+    // }
+            
+             // Replace these values with your proxy server information
+
+
+            // get 10 product from store by id and make in array
+            // ping with proxy for this products  
+
+
+        // $storeurl = "https://leospaw.com/products/ferris-wheel-cat-scratching-board.json";
+        // $proxyHost = '138.219.75.64'; // e.g., 'proxy.example.com':
+        // $proxyPort = 9124; // e.g., 8080
+        // $proxyUsername = 'YYoYwm'; // if your proxy requires authentication
+        // $proxyPassword = 'mPJVXX';
+
+        // $opts = array(
+        //     'http' => array(
+        //         'header' => "User-Agent: MyAgent/1.0\r\n",
+        //         'proxy' => "tcp://{$proxyHost}:{$proxyPort}",
+        //         'request_fulluri' => true,
+        //     ),
+        // );
+
+        // // If your proxy requires authentication, add the following line:
+        // if (!empty($proxyUsername) && !empty($proxyPassword)) {
+        //     $opts['http']['header'] .= "Proxy-Authorization: Basic " . base64_encode("$proxyUsername:$proxyPassword") . "\r\n";
+        // }
+
+        // $context = stream_context_create($opts);
+        // $html = file_get_contents($storeurl, false, $context);
+        // $product = json_decode($html)->product;
+
+        // echo "Product id: " . $product->id . "<br>";
+        // echo "Product Title: " . $product->title . "<br>";
+        // echo "Product updated_at: " . $product->updated_at . "<br>";
+
+
+});
+
+
 
 // Start Queue every 4 Hours
 Route::get('/testaddnewproduct',function (){
